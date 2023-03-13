418b64589275e5e1e2a55962cfad2aaab5120c54
stepanosokin <stepanosokin@gmail.com>
2023-03-13 11:09:17 +0600

    added 'link_id' attribute to 'item' object in vgjson. If you specify the link_id, then inside the dm.links table only the links with this link_id are updated. If not specified, then inside the dm.links table all the links connected to the current dataset_id are updated, which is inconsistent when there are several different links connected to the current dataset. So, if a dataset has severeal links connected to it in dm.links_to_datasets table, you should specify the link_id in the vgjson item.

--- refresh_dataset_links.py
+++ refresh_dataset_links.py
@@ -4,36 +4,37 @@ import os
 import fnmatch
 
 
-
-
 def process_vgjson(filepath, vgdb_pass):
     with open(filepath, 'r') as f:
         vgjson = json.load(f)
         for item in vgjson['data']:
             if 'type' in item.keys():
                 if item['type'] == 'seismic_dataset' and 'id' in item.keys():
-                    update_seismic_link(item['id'], filepath.replace('vgdb.vgjson', ''), vgdb_pass)
+                    update_seismic_link(item, filepath.replace('vgdb.vgjson', ''), vgdb_pass)
                     print(item['id'], filepath.replace('vgdb.vgjson', ''))
 
 
-def update_seismic_link(id, link, vgdb_pass):
+def update_seismic_link(item, link, vgdb_pass):
     with psycopg2.connect(f"dbname=vgdb host=192.168.117.3 user=s.osokin password={vgdb_pass}") as conn:
         cur = conn.cursor()
-        cur.execute(f"""
-        update dm.seismic_datasets set link = '{link}' where dataset_id = {id};
-        update dm.links set link = '{link}' where link_id in (select link_id from dm.links_to_datasets where dataset_id = {id});
-        """)
-
+        if 'link_id' in item.keys():
+            cur.execute(f"""
+                       update dm.seismic_datasets set link = '{link}' where dataset_id = {item['id']};
+                       update dm.links set link = '{link}' where link_id == {item['link_id']};
+                       """)
+        else:
+            cur.execute(f"""
+            update dm.seismic_datasets set link = '{link}' where dataset_id = {item['id']};
+            update dm.links set link = '{link}' where link_id in (select link_id from dm.links_to_datasets where dataset_id = {item['id']});
+            """)
 
 
 def update_links(links_vgjson_path, vgdb_pass):
     with open(links_vgjson_path, 'r') as f:
         links = json.load(f)
-
     for link in links['data']:
         if 'type' in link.keys() and link['type'] == 'link':
             #print(link['value'])
-
             for path, dirs, files in os.walk(os.path.abspath(link['value'])):
                 for filename in fnmatch.filter(files, 'vgdb.vgjson'):
                     filepath = os.path.join(path, filename)
