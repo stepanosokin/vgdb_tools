016fab666489421081c3a9d115fdfc937d896194
stepanosokin <stepanosokin@gmail.com>
2023-03-13 12:50:44 +0600

    1. added update_well_attr_link function to work with well attributes 2. moved psycopg2 connection creation to the update_links function, not to create new connection for each link

--- refresh_dataset_links.py
+++ refresh_dataset_links.py
@@ -4,41 +4,51 @@ import os
 import fnmatch
 
 
-def process_vgjson(filepath, vgdb_pass):
+def process_vgjson(filepath, conn):
     with open(filepath, 'r') as f:
         vgjson = json.load(f)
         for item in vgjson['data']:
             if 'type' in item.keys():
                 if item['type'] == 'seismic_dataset' and 'id' in item.keys():
-                    update_seismic_link(item, filepath.replace('vgdb.vgjson', ''), vgdb_pass)
-                    print(item['id'], filepath.replace('vgdb.vgjson', ''))
+                    update_seismic_link(item, filepath.replace('vgdb.vgjson', ''), conn)
+                    print('seismic_dataset', item['id'], filepath.replace('vgdb.vgjson', ''))
+                elif item['type'] == 'well_attribute' and 'id' in item.keys():
+                    update_well_attr_link(item, filepath.replace('vgdb.vgjson', ''), conn)
+                    print('well_attr', item['id'], filepath.replace('vgdb.vgjson', ''))
 
 
-def update_seismic_link(item, link, vgdb_pass):
-    with psycopg2.connect(f"dbname=vgdb host=192.168.117.3 user=s.osokin password={vgdb_pass}") as conn:
-        cur = conn.cursor()
-        if 'link_id' in item.keys():
-            cur.execute(f"""
-                       update dm.seismic_datasets set link = '{link}' where dataset_id = {item['id']};
-                       update dm.links set link = '{link}' where link_id == {item['link_id']};
-                       """)
-        else:
-            cur.execute(f"""
-            update dm.seismic_datasets set link = '{link}' where dataset_id = {item['id']};
-            update dm.links set link = '{link}' where link_id in (select link_id from dm.links_to_datasets where dataset_id = {item['id']});
-            """)
+def update_seismic_link(item, link, conn):
+    cur = conn.cursor()
+    if 'link_id' in item.keys():
+        cur.execute(f"""
+                   update dm.seismic_datasets set link = '{link}' where dataset_id = {item['id']};
+                   update dm.links set link = '{link}' where link_id == {item['link_id']};
+                   """)
+    else:
+        cur.execute(f"""
+        update dm.seismic_datasets set link = '{link}' where dataset_id = {item['id']};
+        update dm.links set link = '{link}' where link_id in (select link_id from dm.links_to_datasets where dataset_id = {item['id']});
+        """)
+
+
+def update_well_attr_link(item, link, conn):
+    cur = conn.cursor()
+    cur.execute(f"""
+    update dm.links set link = '{link}' where link_id = (select link_id from dm.well_attributes where well_attribute_id = {item['id']});
+    """)
 
 
 def update_links(links_vgjson_path, vgdb_pass):
     with open(links_vgjson_path, 'r') as f:
         links = json.load(f)
-    for link in links['data']:
-        if 'type' in link.keys() and link['type'] == 'link':
-            #print(link['value'])
-            for path, dirs, files in os.walk(os.path.abspath(link['value'])):
-                for filename in fnmatch.filter(files, 'vgdb.vgjson'):
-                    filepath = os.path.join(path, filename)
-                    process_vgjson(filepath, vgdb_pass)
+    with psycopg2.connect(f"dbname=vgdb host=192.168.117.3 user=s.osokin password={vgdb_pass}") as conn:
+        for link in links['data']:
+            if 'type' in link.keys() and link['type'] == 'link':
+                #print(link['value'])
+                for path, dirs, files in os.walk(os.path.abspath(link['value'])):
+                    for filename in fnmatch.filter(files, 'vgdb.vgjson'):
+                        filepath = os.path.join(path, filename)
+                        process_vgjson(filepath, conn)
 
 
 # p = getpass.getpass()
