1b7bbc27825985ed6c61693fc658a1def4fa7fee
stepanosokin <stepanosokin@gmail.com>
2023-04-27 19:12:02 +0600

    First working version od the script

--- download_auction_lic_from_rosnedra.py
+++ download_auction_lic_from_rosnedra.py
@@ -1,10 +1,12 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import requests
 from bs4 import BeautifulSoup
 import locale
 import os, fnmatch
 import shutil
 import pandas as pd
+import openpyxl
+from osgeo import ogr, osr
 
 
 def rus_month_genitive_to_nominative(i_string):
@@ -130,7 +132,10 @@ def download_orders(start=datetime(year=2023, month=1, day=1), end=datetime.now(
                                 os.makedirs(final_directory)
                                 name = search_result_item.find(attrs={'class': 'search-result-link'}).text
                                 with open(f"{final_directory}\\result_url.txt", 'w', encoding='utf-8') as f:
-                                    f.write(f"{name}\n\n{url}")
+                                    f.write(f"{url}")
+                                with open(f"{final_directory}\\result_name.txt", 'w', encoding='utf-8') as f:
+                                    f.write(f"{name}")
+
 
                                 for item_doc_tag in cur_content_tags.find_all('a'):
                                     curl = f"https://www.rosnedra.gov.ru{item_doc_tag['href']}"
@@ -158,48 +163,187 @@ def download_orders(start=datetime(year=2023, month=1, day=1), end=datetime.now(
                             search_result_number += 1
 
 
-def parse_blocks_from_orders(folder='rosnedra_auc'):
+def parse_blocks_from_orders(folder='rosnedra_auc', gpkg='rosnedra_result.gpkg'):
 
     current_directory = os.getcwd()
     directory = os.path.join(current_directory, folder)
+    gpkg_path = os.path.join(directory, gpkg)
 
-    block_id = 0
+    gsk2011_crs = osr.SpatialReference()
+    gsk2011_crs.ImportFromProj4('+proj=longlat +ellps=GSK2011 +towgs84=0.013,-0.092,-0.03,-0.001738,0.003559,-0.004263,0.00739999994614493 +no_defs +type=crs')
+    wgs84_crs = osr.SpatialReference()
+    wgs84_crs.ImportFromProj4('+proj=longlat +datum=WGS84 +no_defs')
+    transform_gsk_to_wgs = osr.CoordinateTransformation(gsk2011_crs, wgs84_crs)
+    gdriver = ogr.GetDriverByName('GPKG')
+    if os.path.exists(gpkg_path):
+        gdriver.DeleteDataSource(gpkg_path)
+    gdatasource = gdriver.CreateDataSource(gpkg_path)
+    out_layer = gdatasource.CreateLayer('license_blocks_rosnedra_orders', srs=wgs84_crs, geom_type=ogr.wkbPolygon)
+    field_names = ['resource_type', 'name', 'area_km', 'reserves_predicted_resources', 'exp_protocol', 'usage_type', 'lend_type', 'planned_terms_conditions', 'source_name', 'source_url', 'order_date']
+    field_types = [ogr.OFTString, ogr.OFTString, ogr.OFTReal, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTDate]
+    for f_name, f_type in zip(field_names, field_types):
+        out_layer.CreateField(ogr.FieldDefn(f_name, f_type))
+    featureDefn = out_layer.GetLayerDefn()
+
+
+    # block_id = 0
+    # ring_id = 0
     for path, dirs, files in os.walk(os.path.abspath(directory)):
-        for filename in fnmatch.filter(files, '*.xls'):
+        for filename in fnmatch.filter(files, '*.xls*'):
+            with open(os.path.join(path, 'result_url.txt')) as uf:
+                curl = uf.read().replace('\n', '')
+            with open(os.path.join(path, 'result_name.txt')) as sf:
+                csource = sf.read().replace('\n', '')
             excel_file = os.path.join(path, filename)
-            # excel_file = 'rosnedra_auc/1_20230425/Приложение к приказу Роснедр от 21.04.2023 № 224.xls'
-            if excel_file[-5:] == '.xlsx':
-                df = pd.read_excel(excel_file, engine='openpyxl')
-            else:
-                df = pd.read_excel(excel_file)
-            # print(df.size)
-            # print(df['Unnamed: 0'])
-            # for cell in df['Unnamed: 0']:
-            #     print(cell)
-            # pass
-            #print(filename, df['Unnamed: 0'][0])
+
+            df = pd.read_excel(excel_file)
+
             nrows, ncols = df.shape
+            block_id = 0
             ring_id = 0
+            cur_ring = ogr.Geometry(ogr.wkbLinearRing)
+            # cur_block_geom = ogr.Geometry(ogr.wkbMultiPolygon)
+            cur_block_geom = ogr.Geometry(ogr.wkbPolygon)
+            # changed_colnums_flag = False
+
+            field_cols = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+            excel_col_nums = {'block_num': 0, 'point_num': 0, 'y_d': 0, 'y_m': 0, 'y_s': 0, 'x_d': 0, 'x_m': 0, 'x_s': 0}
+            excel_col_nums.update(dict(zip(field_names, field_cols)))
+
             for nrow in range(nrows):
-                if df.iloc[nrow, 4] == 1:
+
+                for ncol in range(ncols):
+                    if 'град' in str(df.iloc[nrow, ncol]).replace(' ', '').replace('\n', '') \
+                            and 'мин' in str(df.iloc[nrow, ncol + 1]).replace(' ', '').replace('\n', '') \
+                            and excel_col_nums['y_d'] == 0:
+                        excel_col_nums['y_d'] = ncol
+                        excel_col_nums['y_m'] = ncol + 1
+                        excel_col_nums['y_s'] = ncol + 2
+                        excel_col_nums['x_d'] = ncol + 3
+                        excel_col_nums['x_m'] = ncol + 4
+                        excel_col_nums['x_s'] = ncol + 5
+                        excel_col_nums['point_num'] = ncol - 1
+                        excel_col_nums['area_km'] = ncol - 2
+                    if str(df.iloc[nrow, ncol]).replace(' ', '').replace('\n', '').lower() == 'видполезногоископаемого' and excel_col_nums['resource_type'] == 0:
+                        excel_col_nums['resource_type'] = ncol
+                    if 'наименованиеучастканедр' in str(df.iloc[nrow, ncol]).replace('\n', '').replace(' ', '').lower() and excel_col_nums['name'] == 0:
+                        excel_col_nums['name'] = ncol
+                    if 'ресурсы' in str(df.iloc[nrow, ncol]).replace('\n', '').lower() and excel_col_nums['reserves_predicted_resources'] == 0:
+                        excel_col_nums['reserves_predicted_resources'] = ncol
+                    if 'протокол' in str(df.iloc[nrow, ncol]).replace('\n','').lower() and excel_col_nums['exp_protocol'] == 0:
+                        excel_col_nums['exp_protocol'] = ncol
+                    if 'видпользованиянедрами' in str(df.iloc[nrow, ncol]).replace('\n','').replace(' ','').lower() and excel_col_nums['usage_type'] == 0:
+                        excel_col_nums['usage_type'] = ncol
+                    if 'формапредоставленияучастканедрвпользоание' in str(df.iloc[nrow, ncol]).replace('\n','').replace(' ','').lower()  and excel_col_nums['lend_type'] == 0:
+                        excel_col_nums['lend_type'] = ncol
+                    if 'планируемыесрокипроведения' in str(df.iloc[nrow, ncol]).replace('\n', '').replace(' ', '').lower() and excel_col_nums['planned_terms_conditions'] == 0:
+                        excel_col_nums['planned_terms_conditions'] = ncol
+
+
+
+
+                point_n = df.iloc[nrow, excel_col_nums['point_num']]
+                y_d = df.iloc[nrow, excel_col_nums['y_d']]
+                if str(point_n) == '1' or (cur_ring.GetPointCount() > 2 and not str(y_d).isdigit()):
+                    if ring_id > 0:
+                        if cur_ring.GetPointCount() > 2:
+                            cur_ring.CloseRings()
+                            cur_block_geom.AddGeometry(cur_ring)
                     ring_id += 1
-                if str(df.iloc[nrow, 0]) != 'nan' and type(df.iloc[nrow, 0]) == int and type(df.iloc[nrow, 7]) == float and str(df.iloc[nrow, 7]) != 'nan':
+                    cur_ring = ogr.Geometry(ogr.wkbLinearRing)
+                if str(df.iloc[nrow, excel_col_nums['block_num']]) != 'nan' and len(str(df.iloc[nrow, excel_col_nums['block_num']])) > 0 and str(df.iloc[nrow, excel_col_nums['y_s']]).replace(',', '').replace('.', '').isdigit() and str(df.iloc[nrow, excel_col_nums['y_s']]) != 'nan':
                     #block_id = df.iloc[nrow, 0]
+                    if block_id > 0:
+                        if cur_ring.GetPointCount() > 2:
+                            cur_ring.CloseRings()
+                            cur_block_geom.AddGeometry(cur_ring)
+                        cur_block_geom.CloseRings()
+                        cur_block_geom.Transform(transform_gsk_to_wgs)
+                        feature = ogr.Feature(featureDefn)
+                        feature.SetGeometry(cur_block_geom)
+
+                        for f_name, f_val in zip(field_names, field_vals):
+                            feature.SetField(f_name, f_val)
+                        out_layer.CreateFeature(feature)
+
                     block_id += 1
+
+                    # if str(df.iloc[nrow, -1]) == 'nan' and not changed_colnums_flag:
+                    #     changed_colnums_flag = True
+                    #     for key in excel_col_nums.keys():
+                    #         if excel_col_nums[key] < 0:
+                    #             excel_col_nums[key] = excel_col_nums[key] - 1
+                    #             pass
+                    field_vals = [
+                        df.iloc[nrow, excel_col_nums['resource_type']],
+                        df.iloc[nrow, excel_col_nums['name']],
+                        float(str(df.iloc[nrow, excel_col_nums['area_km']]).replace(',', '.')),
+                        df.iloc[nrow, excel_col_nums['reserves_predicted_resources']],
+                        df.iloc[nrow, excel_col_nums['exp_protocol']],
+                        df.iloc[nrow, excel_col_nums['usage_type']],
+                        df.iloc[nrow, excel_col_nums['lend_type']],
+                        df.iloc[nrow, excel_col_nums['planned_terms_conditions']],
+                        csource,
+                        curl,
+                        datetime.strptime(path[-8:], '%Y%m%d').strftime('%Y-%m-%d')
+                    ]
+                    # if excel_file[-4:] == '.xls':
+                    #     if type(df.iloc[nrow, -5]) == float and str(df.iloc[nrow, -5]) != 'nan':
+                    #         excel_col_nums['reserves_predicted_resources'] = -2
+                    #         excel_col_nums['exp_protocol'] = -1
+                    #         excel_col_nums['usage_type'] = -2
+                    #         excel_col_nums['lend_type'] = -1
+                    #         excel_col_nums['planned_terms_conditions'] = -1
+                    #         pass
+                    #         field_vals = [
+                    #             df.iloc[nrow, excel_col_nums['resource_type']],
+                    #             df.iloc[nrow, excel_col_nums['name']],
+                    #             float(str(df.iloc[nrow, excel_col_nums['area_km']]).replace(',', '.')),
+                    #             df.iloc[nrow, excel_col_nums['reserves_predicted_resources']],
+                    #             df.iloc[2, 0],
+                    #             df.iloc[nrow, excel_col_nums['usage_type']],
+                    #             df.iloc[nrow, excel_col_nums['lend_type']],
+                    #             df.iloc[nrow, excel_col_nums['planned_terms_conditions']],
+                    #             filename,
+                    #             111111
+                    #         ]
+                    # cur_block_geom = ogr.Geometry(ogr.wkbMultiPolygon)
+                    cur_block_geom = ogr.Geometry(ogr.wkbPolygon)
                     ring_id = 1
 
-                if type(df.iloc[nrow, 7]) == float and str(df.iloc[nrow, 7]) != 'nan':
-                    print(filename, f"[block id: {block_id}] [ring id: {ring_id}]" , *df.iloc[nrow, 4:11])
-
-            # for c1_cell in df.iloc[:, 0]:
-            #     # print(c1_cell)
-            #     if c1_cell == 1:
-            #         pass
+                y_d = df.iloc[nrow, excel_col_nums['y_d']]
+                y_m = df.iloc[nrow, excel_col_nums['y_m']]
+                y_s = df.iloc[nrow, excel_col_nums['y_s']]
+                x_d = df.iloc[nrow, excel_col_nums['x_d']]
+                x_m = df.iloc[nrow, excel_col_nums['x_m']]
+                x_s = df.iloc[nrow, excel_col_nums['x_s']]
+                if str(y_s).replace(',', '').replace('.', '').isdigit() and str(df.iloc[nrow, excel_col_nums['y_s']]) != 'nan':
+                    # print(filename, f"[block id: {block_id}] [ring id: {ring_id}]" , *df.iloc[nrow, 4:11])
+                    y = float(str(df.iloc[nrow, excel_col_nums['y_d']]).replace(',', '.')) + \
+                        float(str(df.iloc[nrow, excel_col_nums['y_m']]).replace(',', '.')) / 60 + \
+                        float(str(df.iloc[nrow, excel_col_nums['y_s']]).replace(',', '.')) / 3600
+                    x = float(str(df.iloc[nrow, excel_col_nums['x_d']]).replace(',', '.')) + \
+                        float(str(df.iloc[nrow, excel_col_nums['x_m']]).replace(',', '.')) / 60 + \
+                        float(str(df.iloc[nrow, excel_col_nums['x_s']]).replace(',', '.')) / 3600
+                    # print(x, y)
+                    cur_ring.AddPoint(x, y)
+            if block_id > 0:
+                if cur_ring.GetPointCount() > 2:
+                    cur_ring.CloseRings()
+                    cur_block_geom.AddGeometry(cur_ring)
+                cur_block_geom.CloseRings()
+                cur_block_geom.Transform(transform_gsk_to_wgs)
+                feature = ogr.Feature(featureDefn)
+                feature.SetGeometry(cur_block_geom)
+                for f_name, f_val in zip(field_names, field_vals):
+                    feature.SetField(f_name, f_val)
+                out_layer.CreateFeature(feature)
+            pass
 
 
 
 # download_orders(start=datetime(2023, 1, 1), end=datetime.now(), folder='rosnedra_auc\\')
 
-
-parse_blocks_from_orders()
+# parse_blocks_from_orders()
+pass
 
