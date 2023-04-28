f89fb2efc255e8b64327815048567a77625ae749
stepanosokin <stepanosokin@gmail.com>
2023-04-28 12:35:56 +0600

    First prod version with data download, parse and append to synology

--- download_auction_lic_from_rosnedra.py
+++ download_auction_lic_from_rosnedra.py
@@ -1,12 +1,11 @@
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
 import locale
-import os, fnmatch
-import shutil
+import os, fnmatch, shutil
 import pandas as pd
-import openpyxl
-from osgeo import ogr, osr
+from osgeo import ogr, osr, gdal
+import psycopg2
 
 
 def rus_month_genitive_to_nominative(i_string):
@@ -17,10 +16,11 @@ def rus_month_genitive_to_nominative(i_string):
     return i_string
 
 
-def download_orders(start=datetime(year=2023, month=1, day=1), end=datetime.now(), folder='rosnedra_auc\\'):
+def download_orders(start=datetime(year=2023, month=1, day=1), end=datetime.now(), search_string='Об утверждении Перечня участков недр', folder='rosnedra_auc'):
     current_directory = os.getcwd()
     logdateformat = '%Y-%m-%d %H:%M:%S'
-    log_file = os.path.join(current_directory, f"{folder}logfile.txt")
+    log_file = os.path.join(current_directory, folder, 'logfile.txt')
+    # log_file = os.path.join(log_file, "logfile.txt")
     open(log_file, 'w', encoding='utf-8').close()
     with open(log_file, 'a', encoding='utf-8') as logf:
 
@@ -29,7 +29,7 @@ def download_orders(start=datetime(year=2023, month=1, day=1), end=datetime.now(
             params = {
                 'page': 'search',
                 'step': '1',
-                'q': 'Об утверждении Перечня участков недр'
+                'q': search_string
             }
             headers = {
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -66,7 +66,7 @@ def download_orders(start=datetime(year=2023, month=1, day=1), end=datetime.now(
                         'till_day': datetime.now().strftime('%d'),
                         'till_month': datetime.now().strftime('%m'),
                         'till_year': datetime.now().strftime('%Y'),
-                        'q': 'Об утверждении Перечня участков недр',
+                        'q': search_string,
                         'step': '1',
                         'order': '2',
                         'part': page
@@ -125,8 +125,8 @@ def download_orders(start=datetime(year=2023, month=1, day=1), end=datetime.now(
                                         is_order = True
 
                             if cur_content_tags and is_order:
-                                final_directory = os.path.join(current_directory,
-                                                               f"{folder}{str(search_result_number)}_{item_date.strftime('%Y%m%d')}")
+                                final_directory = os.path.join(current_directory, folder)
+                                final_directory = os.path.join(final_directory, f"{str(search_result_number)}_{item_date.strftime('%Y%m%d')}")
                                 if os.path.exists(final_directory):
                                     shutil.rmtree(final_directory, ignore_errors=True)
                                 os.makedirs(final_directory)
@@ -178,7 +178,8 @@ def parse_blocks_from_orders(folder='rosnedra_auc', gpkg='rosnedra_result.gpkg')
     if os.path.exists(gpkg_path):
         gdriver.DeleteDataSource(gpkg_path)
     gdatasource = gdriver.CreateDataSource(gpkg_path)
-    out_layer = gdatasource.CreateLayer('license_blocks_rosnedra_orders', srs=wgs84_crs, geom_type=ogr.wkbPolygon)
+    # out_layer = gdatasource.CreateLayer('license_blocks_rosnedra_orders', srs=wgs84_crs, geom_type=ogr.wkbPolygon)
+    out_layer = gdatasource.CreateLayer('license_blocks_rosnedra_orders', srs=wgs84_crs, geom_type=ogr.wkbMultiPolygon)
     field_names = ['resource_type', 'name', 'area_km', 'reserves_predicted_resources', 'exp_protocol', 'usage_type', 'lend_type', 'planned_terms_conditions', 'source_name', 'source_url', 'order_date']
     field_types = [ogr.OFTString, ogr.OFTString, ogr.OFTReal, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTString, ogr.OFTDate]
     for f_name, f_type in zip(field_names, field_types):
@@ -204,7 +205,6 @@ def parse_blocks_from_orders(folder='rosnedra_auc', gpkg='rosnedra_result.gpkg')
             cur_ring = ogr.Geometry(ogr.wkbLinearRing)
             # cur_block_geom = ogr.Geometry(ogr.wkbMultiPolygon)
             cur_block_geom = ogr.Geometry(ogr.wkbPolygon)
-            # changed_colnums_flag = False
 
             field_cols = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
             excel_col_nums = {'block_num': 0, 'point_num': 0, 'y_d': 0, 'y_m': 0, 'y_s': 0, 'x_d': 0, 'x_m': 0, 'x_s': 0}
@@ -239,9 +239,6 @@ def parse_blocks_from_orders(folder='rosnedra_auc', gpkg='rosnedra_result.gpkg')
                     if 'планируемыесрокипроведения' in str(df.iloc[nrow, ncol]).replace('\n', '').replace(' ', '').lower() and excel_col_nums['planned_terms_conditions'] == 0:
                         excel_col_nums['planned_terms_conditions'] = ncol
 
-
-
-
                 point_n = df.iloc[nrow, excel_col_nums['point_num']]
                 y_d = df.iloc[nrow, excel_col_nums['y_d']]
                 if str(point_n) == '1' or (cur_ring.GetPointCount() > 2 and not str(y_d).isdigit()):
@@ -268,12 +265,6 @@ def parse_blocks_from_orders(folder='rosnedra_auc', gpkg='rosnedra_result.gpkg')
 
                     block_id += 1
 
-                    # if str(df.iloc[nrow, -1]) == 'nan' and not changed_colnums_flag:
-                    #     changed_colnums_flag = True
-                    #     for key in excel_col_nums.keys():
-                    #         if excel_col_nums[key] < 0:
-                    #             excel_col_nums[key] = excel_col_nums[key] - 1
-                    #             pass
                     field_vals = [
                         df.iloc[nrow, excel_col_nums['resource_type']],
                         df.iloc[nrow, excel_col_nums['name']],
@@ -287,26 +278,6 @@ def parse_blocks_from_orders(folder='rosnedra_auc', gpkg='rosnedra_result.gpkg')
                         curl,
                         datetime.strptime(path[-8:], '%Y%m%d').strftime('%Y-%m-%d')
                     ]
-                    # if excel_file[-4:] == '.xls':
-                    #     if type(df.iloc[nrow, -5]) == float and str(df.iloc[nrow, -5]) != 'nan':
-                    #         excel_col_nums['reserves_predicted_resources'] = -2
-                    #         excel_col_nums['exp_protocol'] = -1
-                    #         excel_col_nums['usage_type'] = -2
-                    #         excel_col_nums['lend_type'] = -1
-                    #         excel_col_nums['planned_terms_conditions'] = -1
-                    #         pass
-                    #         field_vals = [
-                    #             df.iloc[nrow, excel_col_nums['resource_type']],
-                    #             df.iloc[nrow, excel_col_nums['name']],
-                    #             float(str(df.iloc[nrow, excel_col_nums['area_km']]).replace(',', '.')),
-                    #             df.iloc[nrow, excel_col_nums['reserves_predicted_resources']],
-                    #             df.iloc[2, 0],
-                    #             df.iloc[nrow, excel_col_nums['usage_type']],
-                    #             df.iloc[nrow, excel_col_nums['lend_type']],
-                    #             df.iloc[nrow, excel_col_nums['planned_terms_conditions']],
-                    #             filename,
-                    #             111111
-                    #         ]
                     # cur_block_geom = ogr.Geometry(ogr.wkbMultiPolygon)
                     cur_block_geom = ogr.Geometry(ogr.wkbPolygon)
                     ring_id = 1
@@ -338,12 +309,61 @@ def parse_blocks_from_orders(folder='rosnedra_auc', gpkg='rosnedra_result.gpkg')
                 for f_name, f_val in zip(field_names, field_vals):
                     feature.SetField(f_name, f_val)
                 out_layer.CreateFeature(feature)
-            pass
 
 
 
-# download_orders(start=datetime(2023, 1, 1), end=datetime.now(), folder='rosnedra_auc\\')
+def get_latest_order_date_from_synology(pgconn):
+    with pgconn.cursor() as cur:
+        cur.execute("SELECT max(order_date) as latest_order_date FROM rosnedra.license_blocks_rosnedra_orders")
+        ldate = cur.fetchall()[0][0]
+        return datetime(ldate.year, ldate.month, ldate.day)
 
-# parse_blocks_from_orders()
-pass
+
+def update_synology_table(gdalpgcs, folder='rosnedra_auc',  gpkg='rosnedra_result.gpkg'):
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(4326)
+    sourcepath = os.path.join(folder, gpkg)
+    sourceds = gdal.OpenEx(sourcepath, gdal.OF_VECTOR)
+    myoptions = gdal.VectorTranslateOptions(
+        layerName='rosnedra.license_blocks_rosnedra_orders',
+        format='PostgreSQL',
+        accessMode='append',
+        dstSRS=srs,
+        layers=['license_blocks_rosnedra_orders'],
+        geometryType='MULTIPOLYGON'
+    )
+    gdal.VectorTranslate(gdalpgcs, sourceds, options=myoptions)
+
+
+def clear_folder(folder):
+    for root, dirs, files in os.walk(folder):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
+
+
+with open('.pgdsn', encoding='utf-8') as dsnf:
+    dsn = dsnf.read().replace('\n', '')
+
+with psycopg2.connect(dsn) as pgconn:
+    startdt = get_latest_order_date_from_synology(pgconn) + timedelta(days=1)
+
+with open('.pggdal', encoding='utf-8') as gdalf:
+    gdalpgcs = gdalf.read().replace('\n', '')
+
+
+# download_orders(start=startdt, end=datetime.now(), search_string='Об утверждении Перечня участков недр', folder='rosnedra_auc')
+
+# parse_blocks_from_orders(folder='rosnedra_auc', gpkg='rosnedra_result.gpkg')
+
+# update_synology_table(gdalpgcs, folder='rosnedra_auc')
+
+# clear_folder('rosnedra_auc')
+
+
+
+
+
+
 
