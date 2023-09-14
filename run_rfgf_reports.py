00c6ce450d31fed5c02eb5644a6c99dfc141d76b
stepanosokin <stepanosokin@gmail.com>
2023-09-14 20:34:07 +0300

    added run_rfgf_reports.py and run_rfgf_reports

---
+++ run_rfgf_reports.py
@@ -0,0 +1,13 @@
+import json
+from vgdb_reports_rfgf import *
+
+# read the telegram bot credentials
+with open('bot_info_vgdb_bot_toReportsGroup.json', 'r', encoding='utf-8') as f:
+    jdata = json.load(f)
+    bot_info = (jdata['token'], jdata['chatid'])
+
+# read the postgres login credentials for gdal from file
+with open('.pgdsn', encoding='utf-8') as gdalf:
+    dsn = gdalf.read().replace('\n', '')
+
+refresh_rfgf_reports(dsn, pages_pack_size=5000, report_bot_info=bot_info, send_updates=True)
