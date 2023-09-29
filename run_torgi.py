32fbef3d7480699d09f012bc15306fb97dd1f1e6
stepanosokin <stepanosokin@gmail.com>
2023-09-29 10:52:55 +0300

    changed teams channel for torgi reports

--- run_torgi.py
+++ run_torgi.py
@@ -17,7 +17,7 @@ with open('bot_info_vgdb_bot_toStepan.json', 'r', encoding='utf-8') as f:
     # report_bot_info = (jdata['token'], jdata['chatid'])
 
 # # This is telegram credentials to send message to stepanosokin
-with open('license_blocks.webhook', 'r', encoding='utf-8') as f:
-    webhook = f.read().replace('\n', '')
+with open('2023_blocks_nr_ne.webhook', 'r', encoding='utf-8') as f:
+    nr_ne_webhook_2023 = f.read().replace('\n', '')
 
-refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_info, webhook=webhook)
+refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_info, webhook=nr_ne_webhook_2023)
