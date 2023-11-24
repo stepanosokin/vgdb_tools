10e883b0dd47335ea50feab3a8d27a649e7c06f2
stepanosokin <stepanosokin@gmail.com>
2023-11-24 10:15:36 +0300

    testing

--- run_torgi.py
+++ run_torgi.py
@@ -14,10 +14,11 @@ with open('bot_info_vgdb_bot_toAucGroup.json', 'r', encoding='utf-8') as f:
 with open('bot_info_vgdb_bot_toStepan.json', 'r', encoding='utf-8') as f:
     jdata = json.load(f)
     log_bot_info = (jdata['token'], jdata['chatid'])
-    # report_bot_info = (jdata['token'], jdata['chatid'])
+    report_bot_info = (jdata['token'], jdata['chatid'])
 
 # # This is telegram credentials to send message to teams
 with open('2023_blocks_nr_ne.webhook', 'r', encoding='utf-8') as f:
     nr_ne_webhook_2023 = f.read().replace('\n', '')
 
-refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_info, webhook=nr_ne_webhook_2023)
+# refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_info, webhook=nr_ne_webhook_2023)
+refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_info)
