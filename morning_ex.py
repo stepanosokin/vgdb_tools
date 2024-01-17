f780b22ca8c788e5ac990c093fe8e6e894640cbb
stepanosokin <stepanosokin@gmail.com>
2024-01-17 12:10:44 +0300

    physical jerks

--- morning_ex.py
+++ morning_ex.py
@@ -6,5 +6,5 @@ with open('bot_info_vgdb_bot_toJerks.json', 'r', encoding='utf-8') as f:
     bot_info = (jdata['token'], jdata['chatid'])
 
 with open('jerks.log', 'w', encoding='utf-8') as f, requests.Session() as s:
-    message = 'Пора на зарядку!'
+    message = 'Пора на зарядку!\nhttps://open.spotify.com/track/0jpk88zk40MjQ63ljrq7V2?si=cePmfWfpTcuqDM4G-XIvKA'
     send_to_telegram(s, f, bot_info=bot_info, message=message, logdateformat='%Y-%m-%d %H:%M:%S')
