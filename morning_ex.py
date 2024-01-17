51b39723b6a674eace4d3ec026d3182d5a120e98
stepanosokin <stepanosokin@gmail.com>
2024-01-17 11:12:50 +0300

    physical jerks notification

---
+++ morning_ex.py
@@ -0,0 +1,10 @@
+import requests, json
+from vgdb_general import send_to_telegram
+
+with open('bot_info_vgdb_bot_toJerks.json', 'r', encoding='utf-8') as f:
+    jdata = json.load(f)
+    bot_info = (jdata['token'], jdata['chatid'])
+
+with open('jerks.log', 'w', encoding='utf-8') as f, requests.Session() as s:
+    message = 'Пора на зарядку!'
+    send_to_telegram(s, f, bot_info=bot_info, message=message, logdateformat='%Y-%m-%d %H:%M:%S')
