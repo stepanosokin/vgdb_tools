7282a845c3bcdf48f170d3330f0e4cf5704ebae9
stepanosokin <stepanosokin@gmail.com>
2024-01-17 12:24:26 +0300

    physical jerks

--- morning_ex.py
+++ morning_ex.py
@@ -1,10 +1,16 @@
 import requests, json
 from vgdb_general import send_to_telegram
+from datetime import datetime
 
 with open('bot_info_vgdb_bot_toJerks.json', 'r', encoding='utf-8') as f:
     jdata = json.load(f)
     bot_info = (jdata['token'], jdata['chatid'])
 
 with open('jerks.log', 'w', encoding='utf-8') as f, requests.Session() as s:
-    message = 'Пора на зарядку!\nhttps://open.spotify.com/track/0jpk88zk40MjQ63ljrq7V2?si=cePmfWfpTcuqDM4G-XIvKA'
+    secs = datetime.now().isocalendar().week * 10 + 140
+    if secs > 240:
+        secs = 240
+    message = f'Пора на зарядку!\n\n' \
+              f'https://open.spotify.com/track/0jpk88zk40MjQ63ljrq7V2?si=cePmfWfpTcuqDM4G-XIvKA\n\n' \
+              f'Планка {str(secs // 60)}:{str(secs % 60)}'
     send_to_telegram(s, f, bot_info=bot_info, message=message, logdateformat='%Y-%m-%d %H:%M:%S')
