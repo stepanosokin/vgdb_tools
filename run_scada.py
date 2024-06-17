eb67923f261ad68b96d47ee5e412338e3317ceff
stepanosokin <stepanosokin@gmail.com>
2024-06-17 11:48:57 +0300

    tests

--- run_scada.py
+++ run_scada.py
@@ -49,4 +49,4 @@ if data:
         "123": "H2S_4 [мг/м3]"
     }
     if send_to_postgres(pgdsn, 'culture.from_scada', data, channels_dict, bot_info=bot_info):
-        synchro_table([('culture', ['from_scada'])], '.pgdsn', '.ext_pgdsn', bot_info=bot_info, log=False)
+        synchro_table([('culture', ['from_scada'])], '.pgdsn', '.ext_pgdsn', bot_info=bot_info, log=True)
