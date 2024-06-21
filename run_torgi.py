b00cad941092863def96388df1bd84c3c4e538bd
stepanosokin <stepanosokin@gmail.com>
2024-06-21 10:32:46 +0300

    bug fixes

--- run_torgi.py
+++ run_torgi.py
@@ -21,7 +21,11 @@ with open('bot_info_vgdb_bot_toStepan.json', 'r', encoding='utf-8') as f:
 with open('2024_blocks_nr_ne.webhook', 'r', encoding='utf-8') as f:
     nr_ne_webhook_2023 = f.read().replace('\n', '')
 
+with open('.egssh', 'r', encoding='utf-8') as f:
+    egssh = json.load(f)
+
 refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_info, webhook=nr_ne_webhook_2023)
 # refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_info)
 
-synchro_table([('torgi_gov_ru', ['lotcards'])], '.pgdsn', '.ext_pgdsn', bot_info=log_bot_info)
+synchro_table([('torgi_gov_ru', ['lotcards'])], '.pgdsn', '.ext_pgdsn',
+              ssh_host=egssh["host"], ssh_user=egssh["user"], bot_info=log_bot_info)
