bdedea84bb6a5a7b34923c851865a951912dfa8c
stepanosokin <stepanosokin@gmail.com>
2024-06-24 14:09:33 +0300

    tests

--- run_torgi.py
+++ run_torgi.py
@@ -28,4 +28,4 @@ refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_
 # refresh_lotcards(dsn=dsn, log_bot_info=log_bot_info, report_bot_info=report_bot_info)
 
 synchro_table([('torgi_gov_ru', ['lotcards'])], '.pgdsn', '.ext_pgdsn',
-              ssh_host=egssh["host"], ssh_user=egssh["user"], bot_info=log_bot_info)
+              ssh_host=egssh["host"], ssh_user=egssh["user"], bot_info=log_bot_info, local_port_for_ext_pg=5435)
