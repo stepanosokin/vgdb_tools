7b561535782fd867e0d18cf9bad76810ffa47ad0
stepanosokin <stepanosokin@gmail.com>
2025-01-29 14:18:28 +0300

    try to fix wialon script crash after some loops

--- run_wialon_background.py
+++ run_wialon_background.py
@@ -4,28 +4,34 @@ import psycopg2
 import json
 from vgdb_wialon import *
 
-w = wialon_session()
 
-if w:
-    units = get_units(w)
-    if units:
-        update_units_pg('.pgdsn', units)
-        if add_units_to_session(w=w, units=units):
-            pass
-            # for _ in range(500):
-            shrink_events_pg('.pgdsn')
-            while True:
-                try:
-                    data = w.avl_evts()
-                    if data.get('events'):
-                        events=[x for x in data['events'] if x['t'] == 'm']
-                        update_events_pg('.pgdsn', tm=data['tm'], events=events)
-                    sleep(1)
-                except:
-                    try:
-                        w.core_logout()
-                    except:
-                        pass
-                    w = wialon_session()
-                    add_units_to_session(w=w, units=units)
-    w.core_logout()
+
+
+while True:
+    try:
+        w = wialon_session()
+        if w:
+            units = get_units(w)
+            if units:
+                update_units_pg('.pgdsn', units)
+                if add_units_to_session(w=w, units=units):
+                    pass                
+                    shrink_events_pg('.pgdsn')
+                    for _ in range(1000):
+                    # while True:
+                        try:
+                            data = w.avl_evts()
+                            if data.get('events'):
+                                events=[x for x in data['events'] if x['t'] == 'm']
+                                update_events_pg('.pgdsn', tm=data['tm'], events=events)
+                            sleep(1)
+                        except:
+                            try:
+                                w.core_logout()
+                            except:
+                                pass
+                            w = wialon_session()
+                            add_units_to_session(w=w, units=units)
+        w.core_logout()
+    except:
+        pass
